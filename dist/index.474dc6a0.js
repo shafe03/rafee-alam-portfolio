f02dbb0ed8403e6d5e5c46550f43c1aef5b8bcc8
shafe03 <alam.shafee@gmail.com>
2023-03-24 00:09:59 +0600

    completed revied section

---
+++ dist/index.474dc6a0.js
@@ -0,0 +1,2 @@
+let skill_icon_base=document.querySelectorAll(".base"),skill_icon_green=document.querySelectorAll(".green"),skill=document.querySelectorAll(".skill");function responeMouseOver(){const e=e;e.classlist.remove("skill_icon_base")}skill_icon_base.addEventListener("mouseover",responeMouseOver);
+//# sourceMappingURL=index.474dc6a0.js.map
